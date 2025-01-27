29d9aa076c2b86a1d82e32dfc181b49a19958c65
dkrishnamohanpanda209 <dkrishnamohanpanda@gmail.com>
2025-01-27 16:41:58 +0530

    27 jan

---
+++ src/main/java/com/thetestingacademy/pages/pageObjectModel/orangehr/EmployeeListPage.java
@@ -0,0 +1,4 @@
+package com.thetestingacademy.pages.pageObjectModel.orangehr;
+
+public class EmployeeListPage {
+}
---
+++ src/main/java/com/thetestingacademy/pages/pageObjectModel/vwo/LoginPage.java
@@ -0,0 +1,12 @@
+package com.thetestingacademy.pages.pageObjectModel.vwo;
+
+import org.openqa.selenium.By;
+
+public class LoginPage {
+//    page class
+//    1) page locators
+//    2) page
+
+//    step-1 pagelocators
+    private By  username = By.id("login-username");
+}
